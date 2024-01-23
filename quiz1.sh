d8c5b572b2c9c319f93e005904c3672e2cdb21c2
Roger Xu <rogerxu00@campus-002-071.ucdavis.edu>
2024-01-23 09:37:14 -0800

    new

--- quiz1.sh
+++ quiz1.sh
@@ -1,10 +1,13 @@
 echo "Roger Xu"
 echo "$USER"
 
-gunzip -c ~/Code/MCB185/data/dictionary.gz | grep "a" | grep -v -E "[^muoacft]+" | grep -E ".{4,}" | wc -l
-gunzip -c ~/Code/MCB185/data/dictionary.gz | grep "b" | grep -v -E "[^tairnl]+" | grep -E ".{4,}" | wc -l
-gunzip -c ~/Code/MCB185/data/dictionary.gz | grep "c" | grep -v -E "[^maodin]+" | grep -E ".{4,}" | wc -l
-gunzip -c ~/Code/MCB185/data/dictionary.gz | grep "z" | grep -v -E "[^anoigr]+" | grep -E ".{4,}" | wc -l
+gunzip -c ~/Code/MCB185/data/dictionary.gz | grep "a" | grep -v -E "[^muoacft]" | grep -E ".{4,}" | wc -l
+
+gunzip -c ~/Code/MCB185/data/dictionary.gz | grep "b" | grep -v -E "[^btairnl]" | grep -E ".{4,}" | wc -l
+
+gunzip -c ~/Code/MCB185/data/dictionary.gz | grep "c" | grep -v -E "[^cmaodin]" | grep -E ".{4,}" | wc -l
+
+gunzip -c ~/Code/MCB185/data/dictionary.gz | grep "z" | grep -v -E "[^zanoigr]" | grep -E ".{4,}" | wc -l
 
 gunzip -c ~/Code/MCB185/data/jaspar2024_core.transfac.gz | grep -v "^#" | cut -f 3 | sort | uniq -c | sort -n | grep "tax_group"
 
