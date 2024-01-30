29c607c1a1f0d26ef80c19c587fa42a6e230f079
Roger Xu <rogerxu00@Rogers-MacBook.local>
2024-01-29 17:05:05 -0800

    new

--- 22oligotemp.py
+++ 22oligotemp.py
@@ -3,8 +3,8 @@
 import math
 
 def oligotemp(A, C, G, T):
-	if (A + T + G + C) <= 13: return ((A + T) * 2) + ((G + C) * 4)
-	else: return (64.9 + 41 * (G + C - 16.4)) / (A + T + G + C)
+	if(A + T + G + C) <= 13: return((A + T) * 2) + ((G + C) * 4)
+	else: return(64.9 + 41 * (G + C - 16.4)) / (A + T + G + C)
 	
 print(oligotemp(1, 2, 3, 4))
 print(oligotemp(2, 3, 4, 5))
